4f1dd7066f7458f9be487f744a6f72d046190d88
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-08 15:41:52 +0300

    Create bisikletKiralama.js

---
+++ bisikletKiralama.js
@@ -0,0 +1,17 @@
+let bisikletKiralama ={
+  ad:"Can",
+  soyad:"Ata",
+  gunFiyat:20,
+  kacGun:4,
+  borcHesapla:function(){console.log("Toplam Fiyat :  " + (this.gunFiyat * this.kacGun));}
+}
+
+bisikletKiralama.borcHesapla()let bisikletKiralama ={
+  ad:"Can",
+  soyad:"Ata",
+  gunFiyat:20,
+  kacGun:4,
+  borcHesapla:function(){console.log("Toplam Fiyat :  " + (this.gunFiyat * this.kacGun));}
+}
+
+bisikletKiralama.borcHesapla()
